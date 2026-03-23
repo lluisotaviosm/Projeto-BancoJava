9e905d11a6efbadc38b2c71176e2eba2c942285d
luisdoscod <luisotavio70p@gmail.com>
2026-03-23 00:51:06 -0300

    feat: adicionar lógica de bloqueio após 3 tentativas de senha

--- src/Funcionario.java
+++ src/Funcionario.java
@@ -1,13 +1,26 @@
 public class Funcionario implements Autenticavel{
     private String nome;
     private String senha;
+    private int tentativas = 0;
+    private boolean bloqueado = false;
 
     public Funcionario(String nome, String senha) {
         this.nome = nome;
         this.senha = senha;
     }
     public boolean autenticar(String senhaInformada){
-        return this.senha.equals(senhaInformada);
+        if (bloqueado) {
+            System.out.println("ACESSO NEGADO: Usuário bloqueado.");
+            return false;
+        }
+        if (this.senha.equals(senhaInformada)) {
+            tentativas = 0;
+            return true;
+        } else {
+            tentativas++;
+            if (tentativas >= 3) bloqueado = true;
+            return false;
+        }
     }
     public String getNome(){return nome; }
 }
